edd89d71218ca5bf736b7cf9e4c1d1374924925e
xxdznl <1756775636@qq.com>
2024-10-15 21:47:30 +0800

    initt

--- apps/webget.cc
+++ apps/webget.cc
@@ -11,6 +11,8 @@ void get_URL( const string& host, const string& path )
 {
   cerr << "Function called: get_URL(" << host << ", " << path << ")\n";
   cerr << "Warning: get_URL() has not been implemented yet.\n";
+  cerr << "Warning: get_URL() has not been implemented yet.\n";
+
 }
 
 int main( int argc, char* argv[] )
